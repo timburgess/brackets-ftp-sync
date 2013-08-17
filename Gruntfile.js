e5a5342e0d42157f8ecd22b5cbd28abe701da247
Tim Burgess <timburgess@mac.com>
2013-08-17 16:01:44 +1000

    Grunt now copies to build & mangles main.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,9 @@
 
 //glifyjs ../../node/ftpDomain.js --mangle --reserved 'require' --screw-ie8 -o ftpDomain.js
 
+/*jslint indent: 4, white:true */
+/*global module */
+
 module.exports = function(grunt) {
 
   // Project config
@@ -9,21 +12,36 @@ module.exports = function(grunt) {
 
     pkg: grunt.file.readJSON('package.json'),
 
+    copy: {
+      main: {
+          expand: true,
+          cwd: 'src/',
+          src: '**',
+          dest: 'build/'
+      }
+    },
+
     uglify: {
       options: {
+          mangle: {
+              except: ['$', 'require']
+          },
+          'screw-ie8': true
       },
       build: {
-        src: 'src/main.js',
-        dest: 'build/main.js'
+          src: 'src/main.js',
+          dest: 'build/src/main.js'
       }
     }
   });
 
-  // load uglify plugin
+  // load copy plugin
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-clean');
 
   // default
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['copy']);
 
 
 };
---
+++ README.md
@@ -0,0 +1,1 @@
+Contact: timburgess@mac.com
